c8cfa0aeb2e4afe23af7465f09477b17dc09ad92
Vivian Cardoso <viviancardoso124@outlook.com>
2024-09-09 22:04:39 -0300

    Atualizando metodos do User Service

--- src/main/java/br/com/fiap/settings/service/UserService.java
+++ src/main/java/br/com/fiap/settings/service/UserService.java
@@ -1,14 +1,20 @@
 package br.com.fiap.settings.service;
 
+import br.com.fiap.settings.dto.MailResponse;
 import br.com.fiap.settings.model.User;
 import br.com.fiap.settings.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import br.com.fiap.settings.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private MailService mailService;
+
+
     @Autowired
     private UserRepository userRepository;
 
@@ -16,11 +22,21 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public User update(User user) {
+        User targetUser = findById(user.getId());
+        return userRepository.save(targetUser);
+    }
+
     public void delete(Long userId) {
         userRepository.deleteById(userId);
     }
+
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new RuntimeException("Pessoa não encontrada."));
     }
+
+//    public Page<MailResponse> findAllMails(Pageable pageable, Long userId) {
+//        return mailService.findAllByUserId(pageable, userId);
+//    }
 }
 
