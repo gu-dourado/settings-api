e58659d545fdfe27099e00119cbe3a36346f5dc5
Vivian Cardoso <viviancardoso124@outlook.com>
2024-09-09 21:41:36 -0300

    Adicionando metodos do User Service

--- src/main/java/br/com/fiap/settings/service/UserService.java
+++ src/main/java/br/com/fiap/settings/service/UserService.java
@@ -1,7 +1,21 @@
 package br.com.fiap.settings.service;
 
+import br.com.fiap.settings.model.User;
 import org.springframework.stereotype.Service;
+import br.com.fiap.settings.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long userId) {
+        userRepository.deleteById(userId);
+    }
 }
+
