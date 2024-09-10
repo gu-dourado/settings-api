299133f15a81bdece6fcfd01ae568d4e3b42467f
Npx <cheyprivado@gmail.com>
2024-09-09 23:06:34 -0300

    ajustes para fluxo do user

--- src/main/java/br/com/fiap/settings/repository/MailRepository.java
+++ src/main/java/br/com/fiap/settings/repository/MailRepository.java
@@ -1,8 +1,9 @@
 package br.com.fiap.settings.repository;
 
 import br.com.fiap.settings.model.Mail;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MailRepository extends JpaRepository<Mail, Long> {
-
 }
